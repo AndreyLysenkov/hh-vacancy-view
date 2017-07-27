fe14b1a6f0d66f282da0c9ba932d065ac84838b5
allan_walpy <andreylysenkovallan@gmail.com>
2017-07-27 19:09:06 +0300

    добавил классы для Json parsing

---
+++ core/papse/TypeParse.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Vacancy.Core
+{
+
+    public class TypeParse
+    {
+
+        public string Id;
+
+        public string Name;
+
+    }
+
+}
